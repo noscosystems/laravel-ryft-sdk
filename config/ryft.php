123383915ba200b22b46a9267add45e7403ba9e3
Puttiong Ander Vidal <26520527+pavidal@users.noreply.github.com>
2025-03-05 15:48:33 +0000

    Added package config file

--- config/ryft.php
+++ config/ryft.php
@@ -2,5 +2,22 @@
 
 // config for Nosco/Ryft
 return [
+    'auth' => [
+        'public' => env('RYFT_PUBLIC_KEY'),
+        'private' => env('RYFT_PRIVATE_KEY'),
+    ],
 
+    'sandbox' => env('RYFT_SANDBOX', false),
+
+    'rate_limit' => [
+        'enabled' => env('RYFT_RATE_LIMIT_ENABLED', true),
+        'limit' => [
+            'production' => env('RYFT_RATE_LIMIT_PRODUCTION', 25),
+            'sandbox' => env('RYFT_RATE_LIMIT_SANDBOX', 5),
+        ],
+        'window' => env('RYFT_RATE_LIMIT_WINDOW', 1),
+        'backoff' => [2, 4, 8, 16, 32, 64, 128, 256, 512],
+    ],
+
+    'currency' => env('RYFT_CURRENCY', env('APP_CURRENCY', 'USD')),
 ];
