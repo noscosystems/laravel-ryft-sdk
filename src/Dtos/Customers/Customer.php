c807a27c60b0e82498298c4c93d6ba9b3538e588
Puttiong Ander Vidal <26520527+pavidal@users.noreply.github.com>
2025-04-30 13:59:50 +0100

    Added customer DTOs

---
+++ src/Dtos/Customers/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Nosco\Ryft\Dtos\Customers;
+
+use DateTimeInterface;
+use Illuminate\Support\Collection;
+use Nosco\Ryft\Dto;
+
+readonly class Customer extends Dto
+{
+    public function __construct(
+        public ?string $id = null,
+        public ?string $email = null,
+        public ?string $firstName = null,
+        public ?string $lastName = null,
+        public ?string $homePhoneNumber = null,
+        public ?string $mobilePhoneNumber = null,
+        public ?Collection $metadata = null,
+        public ?string $defaultPaymentMethod = null,
+        public ?DateTimeInterface $createdTimestamp = null,
+    ) {}
+}
